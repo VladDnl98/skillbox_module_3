00f0b9f9ae31a085859c14a980a03917976fe3ff
VladDnl98 <86232836+VladDnl98@users.noreply.github.com>
2021-10-20 21:51:09 +0300

    "master"

--- lib/main.dart
+++ lib/main.dart
@@ -27,63 +27,43 @@ class Home extends StatefulWidget {
 //Задание 1
 class _HomeState extends State<Home> {
   final String assetName = ('assets/svg/circle.svg');
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text("SVG"),
-      // ),
-      // ignore: avoid_unnecessary_containers
-      body: Container(
-        child: Stack(children: [
-          Positioned(
-            top: -40,
-            left: -40,
-            height: 250,
-            width: 250,
-            child: SvgPicture.asset(assetName,
-                semanticsLabel: 'assets/svg/circle.svg'),
-          ),
-          // SizedBox(
-          //   height: 100,
-          //   width: 100,
-          //   child: SvgPicture.asset(assetName,
-          //       semanticsLabel: 'assets/svg/circle.svg'),
-          // ),
-        ]),
-      ),
-    );
-  }
-}
-
-// Задание 2
-class Carusel extends StatelessWidget {
   final List<String> imageList = [
     'assets/image/lion.jpg'
         'assets/image/lemour.jpg'
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Image Carousel Example '),
-      ),
-      body: CarouselSlider(
-        options: CarouselOptions(
-          autoPlay: true,
+        appBar: AppBar(
+          title: const Text('Flutter SVG'),
         ),
-        items: imageList
-            .map(
-              (item) => Center(
-                child: Image.asset(
-                  item,
-                  fit: BoxFit.cover,
-                ),
+        body: Container(
+            child: Stack(children: [
+              Positioned(
+                bottom: -40,
+                right: -40,
+                height: 250,
+                width: 250,
+                child: SvgPicture.asset(assetName,
+                    semanticsLabel: 'assets/svg/circle.svg'),
               ),
-            )
-            .toList(),
-      ),
-    );
+            ]),
+            body: CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+              ),
+              items: imageList
+                  .map(
+                    (item) => Center(
+                      child: Image.asset(
+                        ('assets/image/lion.jpg'),
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            )));
   }
 }
